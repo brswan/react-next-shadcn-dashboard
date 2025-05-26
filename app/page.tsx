c38ca8cf4399f9d5ccf614dd552351e0361ea5d8
brswan <bryanrs65@gmail.com>
2025-05-26 11:24:29 -0400

    adding grid stub

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div className=''></div>;
+  return (
+    <div className='grid grid-col-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4'>
+      <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
+        Test
+      </div>
+      <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2'>
+        Test
+      </div>
+      <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
+        Test
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
