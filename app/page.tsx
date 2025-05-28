5a86331f935726aa87967b3b52399bb7c79b10f4
brswan <bryanrs65@gmail.com>
2025-05-27 20:54:23 -0400

    Added charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 import React from "react";
 
 const Homepage = () => {
   return (
     <div className='grid grid-col-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4'>
       <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
-        Test
+        <AppBarChart />
       </div>
       <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
-      <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
+      <div className='bg-primary-foreground p-4 rounded-lg'>
+        <AppPieChart />
+      </div>
       <div className='bg-primary-foreground p-4 rounded-lg'>Test</div>
       <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2'>
-        Test
+        <AppAreaChart />
       </div>
       <div className='bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2'>
         Test
