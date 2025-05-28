d6f39faee9fe7904b7982003378dde1d1ccc1422
brswan <bryanrs65@gmail.com>
2025-05-27 20:55:49 -0400

    updates to pie chart

--- components/AppPieChart.tsx
+++ components/AppPieChart.tsx
@@ -8,6 +8,7 @@ import {
   ChartTooltipContent,
 } from "./ui/chart";
 import { Label, Pie, PieChart } from "recharts";
+import { TrendingUp } from "lucide-react";
 
 const chartData = [
   { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
@@ -96,6 +97,15 @@ const AppPieChart = () => {
           </Pie>
         </PieChart>
       </ChartContainer>
+      <div className='mt-4 flex flex-col gap-2 items-center'>
+        <div className='flex items-center gap-2 font-medium leading-none'>
+          Trending up by 5.2% this month{" "}
+          <TrendingUp className='h-4 w-4 text-green-500' />
+        </div>
+        <div className='leading-none text-muted-foreground'>
+          Showing total visitors for the last 6 months
+        </div>
+      </div>
     </div>
   );
 };
